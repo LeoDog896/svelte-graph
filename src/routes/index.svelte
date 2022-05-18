8e145f968f7025543dd7a6bcf69bd88db5561250
LeoDog896 <LeoDog896@hotmail.com>
2022-05-18 14:22:47 +0000

    Add base canvas variable

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let canvas: HTMLCanvasElement;
+
+  onMount(() => {
+    const context = canvas.getContext("2d")
+
+    if (!context) return
+
+    context.moveTo(0, 0);
+    context.lineTo(200, 200);
+  });
+</script>
+
+<canvas bind:this={canvas}></canvas>
