99d82dd8c366734bc9c1ba692d2edbf2c0120f9c
LeoDog896 <LeoDog896@hotmail.com>
2022-05-18 21:25:44 +0000

    Begin svelte graph

---
+++ src/lib/Graph.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  interface Position {
+    x: number;
+    y: number;
+  }
+
+  export let center: Position = { x: 0, y: 0 };
+  export let xMin = -10;
+  export let xMax = 10;
+  export let yMin = -10;
+  export let yMax = 10;
+  /** Major step (units) */
+  export let stepMajor = 1;
+  export let minorAmount = 1
+
+  let canvas: HTMLCanvasElement;
+
+  onMount(() => {
+    const context = canvas.getContext("2d")
+
+    if (!context) return
+
+    context.strokeStyle = "black";
+
+    context.beginPath();
+    context.moveTo(0, 0);
+    context.lineTo(200, 0);
+    context.stroke();
+  });
+</script>
+
+<canvas bind:this={canvas}></canvas>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,5 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-
-  let canvas: HTMLCanvasElement;
-
-  onMount(() => {
-    const context = canvas.getContext("2d")
-
-    if (!context) return
-
-    context.moveTo(0, 0);
-    context.lineTo(200, 200);
-  });
+  import Graph from "$lib/Graph.svelte"
 </script>
 
-<canvas bind:this={canvas}></canvas>
+<Graph/>
